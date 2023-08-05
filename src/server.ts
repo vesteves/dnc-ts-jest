c167882aa504025526321c4fbcc4261e7172e45a
Vitor Esteves <vitordasilvaesteves@gmail.com>
2023-08-05 15:07:18 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import * as express from 'express'
+import * as cors from 'cors'
 import produtoRoute from './module/produto/produto.route'
 
 const app = express();
 app.use(express.json());
+app.use(cors())
+app.options('*', cors())
 
 app.use('/produto', produtoRoute);
 
